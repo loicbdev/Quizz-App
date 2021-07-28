df49add1c6b9b299e1947fd2c39fd2532989235d
loicbdev <loicbodolec@msn.com>
2021-07-28 14:50:01 +0200

    first commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,42 @@
+export const Questions = [
+  {
+    prompt: "Which of this is not a relational database management system (RDBMS)?",
+    optionA: "MySQL",
+    optionB: "Microsoft SQL Server",
+    optionC: "PostgreSQL",
+    optionD: "MongoDB",
+    answer: "optionD",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    answer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "Java",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "React",
+    answer: "optionA",
+  },
+  {
+    prompt: "What is the result of typeof(null)?",
+    optionA: "array",
+    optionB: "undefined",
+    optionC: "object",
+    optionD: "null",
+    answer: "optionC",
+  },
+  {
+    prompt: "Who is the creator of JavaScript?",
+    optionA: "Michael Jackson",
+    optionB: "Bremdan Eich",
+    optionC: "Tim Berners-Lee",
+    optionD: "Albert Einstein",
+    answer: "optionB",
+  },
+];
